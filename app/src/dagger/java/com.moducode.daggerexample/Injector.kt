7aa9cee6233138fb0afdd3dc22f7c4ea030be75d
weullerk <weuller.krysthian@gmail.com>
2019-09-25 10:03:20 -0300

    Adding components

--- app/src/dagger/java/com.moducode.daggerexample/Injector.kt
+++ app/src/dagger/java/com.moducode.daggerexample/Injector.kt
@@ -1,13 +1,32 @@
 package com.moducode.daggerexample
 
+import com.moducode.daggerexample.dagger.AppComponent
+import com.moducode.daggerexample.dagger.ContextModule
+import com.moducode.daggerexample.dagger.DaggerAppComponent
 import com.moducode.daggerexample.ui.fragment.EpisodeDetailFragment
+import com.moducode.daggerexample.ui.fragment.EpisodeDetailPresenter
 import com.moducode.daggerexample.ui.fragment.EpisodeListFragment
+import com.moducode.daggerexample.ui.fragment.EpisodeListPresenter
 import com.moducode.daggerexample.ui.fragment.contract.EpisodeDetailContract
 import com.moducode.daggerexample.ui.fragment.contract.EpisodeListContract
+import dagger.internal.DaggerCollections
 
-fun EpisodeDetailFragment.buildPresenter(): EpisodeDetailContract.Actions = TODO()
+fun EpisodeDetailFragment.buildPresenter(): EpisodeDetailPresenter {
+    val component = DaggerAppComponent.builder()
+            .contextModule(ContextModule(requireContext()))
+            .build()
 
-fun EpisodeListFragment.buildPresenter(): EpisodeListContract.Actions = TODO()
+    return EpisodeDetailPresenter(component.dbRepo(), component.schedulers())
+
+}
+
+fun EpisodeListFragment.buildPresenter(): EpisodeListPresenter {
+    val component = DaggerAppComponent.builder()
+            .contextModule(ContextModule(requireContext()))
+            .build()
+
+    return EpisodeListPresenter(component.episodeService(), component.schedulers(), component.dbRepo())
+}
 
 
 
